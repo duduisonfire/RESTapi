8ed66f5511eab1b0f6a0831f7c4af1e27f0b8306
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2022-09-28 04:02:34 -0300

    Deleting dist

--- dist/config/app.js
+++
@@ -1,3 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});exports. default = {
-  url: `http://${process.env.URL}:${process.env.PORT}`,
-};
--- dist/routes/photo.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
-var _PhotoController = require('../controllers/PhotoController'); var _PhotoController2 = _interopRequireDefault(_PhotoController);
-var _login = require('../middlewares/login'); var _login2 = _interopRequireDefault(_login);
-
-const router = new (0, _express.Router)();
-
-router.post('/', _login2.default, _PhotoController2.default.store);
-
-exports. default = router;
